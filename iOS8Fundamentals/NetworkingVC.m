cd079067d736721079ce5364a5b7690c87d10e83
Pedro Ontiveros <pontiveros@pedrostudio4.local>
2015-09-18 15:11:32 -0300

    update upload image to server using nsurlconnection.

--- iOS8Fundamentals/NetworkingVC.m
+++ iOS8Fundamentals/NetworkingVC.m
@@ -55,10 +55,15 @@
 {
     self.imageHelper = [[PickerImageHelper alloc] initWithViewController:self];
     __weak NetworkingVC *pSelf = self;
+    __weak UIButton *button = (UIButton*)sender;
     
     [self.imageHelper selectImageWithCompletionHandler:^(UIImage *image, NSError *error) {
         if (image) {
-            [pSelf sendImageByNSURLSession2:image];
+            if (button.tag == 1) {
+                [pSelf sendImageByNSURLSession:image];
+            } else {
+                [pSelf sendImageByNSURLConnection:image];
+            }
         } else {
             NSLog(@"Error: %@", [error description]);
         }
@@ -67,80 +72,67 @@
 
 - (void)sendImageByNSURLConnection:(UIImage*)image
 {
+    NSString *stringURL = @"http://localhost/uploadfiledashboard/uploadfile.php";
+    NSData   *imageData = UIImageJPEGRepresentation(image, .6);
+    NSString   *boudary = @"0xKhTmLbOuNdArY";
+    
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setCachePolicy:NSURLRequestReloadIgnoringCacheData];
+    [request setHTTPShouldHandleCookies:NO];
+    [request setTimeoutInterval:30.0];
+    [request setHTTPMethod:@"POST"];
     
+    // Set Content-Type in HTTP Header
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boudary];
+    [request setValue:contentType forHTTPHeaderField:@"Content-Type"];
     
+    // Post Body
+    NSMutableData *body = [NSMutableData data];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boudary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"uploadfile\";filename=\"image.jpg\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"Content-Type: image/jpeg\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:imageData];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", boudary] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    [request setHTTPBody:body]; // Set body to Request
+    [request setURL:[NSURL URLWithString:stringURL]]; // Set url to request.
+    
+    __weak NetworkingVC *pSelf = self;
     [NSURLConnection sendAsynchronousRequest:request
                                        queue:[[NSOperationQueue alloc] init]
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+                               if (!error) {
+                                   NSString *payload = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                                   NSLog(@"RESPONSE PAYLOAD: %@", payload);
+                               } else {
+                                   NSLog(@"ERROR: %@", [error description]);
+                               }
                                
+                               UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Network"
+                                                                                              message:@"Connection has finished."
+                                                                                       preferredStyle:UIAlertControllerStyleAlert];
+                               
+                               UIAlertAction *action = [UIAlertAction actionWithTitle:@"Accept"
+                                                                                style:UIAlertActionStyleDefault
+                                                                              handler:nil];
+                               [alert addAction:action];
+                               [pSelf presentViewController:alert animated:YES completion:nil];
                            }];
 }
 
 - (void)sendImageByNSURLSession:(UIImage*)image
 {
-    NSData *imageData = UIImageJPEGRepresentation(image, 0.6);
-    
-    // 1
-    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
-    config.HTTPMaximumConnectionsPerHost = 1;
-    [config setHTTPAdditionalHeaders:@{@"Content-Type":@"application/json"}];
-//    [config setHTTPAdditionalHeaders:@{@"Authorization": [Dropbox apiAuthorizationHeader]}];
-    
-    // 2
-    NSURLSession *upLoadSession = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:nil];
-    
-    // for now just create a random file name, dropbox will handle it if we overwrite a file and create a new name..
-    NSURL *url = [NSURL URLWithString:@"http://localhost/storemobile"];
-    
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
-    [request setHTTPMethod:@"POST"];
-    
-    // 3
-    NSURLSessionUploadTask *uploadTask = [upLoadSession uploadTaskWithRequest:request
-                                                                     fromData:imageData
-                                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                                                [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-                                                                if (!error) {
-                                                                    NSString *payload = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                                                                    NSLog(@"RESPONSE: %@", [response description]);
-                                                                    NSLog(@"PAYLOAD: %@", payload);
-                                                                } else {
-                                                                    NSLog(@"ERROR: %@", [error description]);
-                                                                }
-                                                            }];
-    
-    // 4
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    
-    // 5
-    [uploadTask resume];
-}
-- (void)sendImageByNSURLSession2:(UIImage*)image
-{
-//    NSString *deviceID = [[NSUserDefaults standardUserDefaults] objectForKey:@"deviceID"];
-//    NSString *textContent = @"This is a new note";
-    
     // Build the request body
     NSString *boundary = @"-------------------123456789000000";
     NSMutableData *body = [NSMutableData data];
-//    // Body part for "deviceId" parameter. This is a string.
-//    [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-//    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", @"deviceId"] dataUsingEncoding:NSUTF8StringEncoding]];
-//    [body appendData:[[NSString stringWithFormat:@"%@\r\n", deviceID] dataUsingEncoding:NSUTF8StringEncoding]];
-//    // Body part for "textContent" parameter. This is a string.
-//    [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-//    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", @"textContent"] dataUsingEncoding:NSUTF8StringEncoding]];
-//    [body appendData:[[NSString stringWithFormat:@"%@\r\n", textContent] dataUsingEncoding:NSUTF8StringEncoding]];
+
     // Body part for the attachament. This is an image.
     NSData *imageData = UIImageJPEGRepresentation(image, 0.6);
     
     if (imageData) {
         [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"image.jpg\"\r\n", @"image"] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"Content-Type: multipart/form-data; boundary=%@", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-//        [body appendData:[[NSString stringWithFormat:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Type: image/jpeg;"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"image.jpg\"\r\n", @"uploadfile"] dataUsingEncoding:NSUTF8StringEncoding]];
         [body appendData:imageData];
         [body appendData:[[NSString stringWithFormat:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
     }
@@ -148,19 +140,14 @@
     
     // Setup the session
     NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    sessionConfiguration.HTTPAdditionalHeaders = @{
-//                                                   @"api-key"       : @"55e76dc4bbae25b066cb",
-//                                                   @"Accept"        : @"application/json",
-//                                                   @"Content-Type"  : [NSString stringWithFormat:@"application/octet-stream; boundary=%@", boundary]
-                                                   @"Content-Type"  : [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary]
-                                                   };
+    sessionConfiguration.HTTPAdditionalHeaders = @{@"Content-Type":[NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary]};
     
     // Create the session
     // We can use the delegate to track upload progress
     NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration delegate:nil delegateQueue:nil];
     
     // Data uploading task. We could use NSURLSessionUploadTask instead of NSURLSessionDataTask if we needed to support uploads in the background
-    NSURL *url = [NSURL URLWithString:@"http://localhost/storemobile"];
+    NSURL *url = [NSURL URLWithString:@"http://localhost/uploadfiledashboard/uploadfile.php"];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     request.HTTPMethod = @"POST";
     request.HTTPBody   = body;
